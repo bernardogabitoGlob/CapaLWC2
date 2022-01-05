65f7bdedf9c99cdf4b59ce1d4053475b513d09dc
bgabitobrodsky <bgabitobrodsky@frba.utn.edu.ar>
2022-01-05 16:54:42 -0300

    front: header y buscador

--- force-app/main/default/lwc/pokedex/pokedex.js
+++ force-app/main/default/lwc/pokedex/pokedex.js
@@ -1,5 +1,12 @@
 import { LightningElement } from 'lwc';
 
 export default class Pokedex extends LightningElement {
+
     
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            this.queryTerm = evt.target.value;
+        }
+    }
 }
